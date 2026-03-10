b12b0ff4d1b443d66b688f117a312aa8cce8cf3e
Bhaumik Raja <webdevbhaumik@gmail.com>
2026-03-10 15:51:27 -0400

    Rendered first chart widget on homepage correctly - market overview

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -21,6 +21,7 @@ const TradingViewWidget = ({title, scriptUrl, config, height = 600, className} :
             {title && <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>}
             <div className={cn('tradingview-widget-container', className)} ref={containerRef}>
                 <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }} />
+                <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener nofollow" target="_blank"><span className="blue-text">AAPL stock chart</span></a><span className="trademark"> by TradingView</span></div>
             </div>
         </div>
     );
